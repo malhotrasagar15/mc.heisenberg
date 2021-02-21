288cea5a41811e9e2236585799b1f264e325c590
aman <53216120+amannarya@users.noreply.github.com>
2021-02-21 19:29:04 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 21 10:54:30 2021
-
-@author: Aman Arya
-"""
 import random
 import math
 import numpy as np
@@ -20,7 +14,7 @@ func()
 n=10.0
 lattice_initial = [[[ func() for i in range(n) ] for j in range(n) ] for k in range(n)]
 
-def ising_model(n,tem,simulaions,J):      #tem=temperature
+def ising_model(n,tem,simulaions,J):      
     energy_tem = []                               
     n_cube = n*n*n
     
@@ -28,23 +22,23 @@ def ising_model(n,tem,simulaions,J):      #tem=temperature
         energy = []
         energy_sq = []
         
-        lattice = lattice_initial(n)   #initializing the lattice
+        lattice = lattice_initial(n)   
         size = len(lattice[0])
        
         for simulation in range(simulaions): 
-            for l in range(n_cube):           #Each simulation is (n^2) spin flips acc to mcmc rule
+            for l in range(n_cube):           
                 x = random.randint(0,n-1)
                 y = random.randint(0,n-1)
                 z = random.randint(0,n-1)
-                                                            #cal. change in energy
+                                                           
                 DeltaE = 2*lattice[x,y,z]*J*(lattice[x,(y-1)%size,z] + lattice[x,(y+1)%size,z] + lattice[(x-1)%size,y,z] + lattice[(x+1)%size,y,z] + lattice[x,y,(z+1)%size] + lattice[x,y,(z-1)%size])
-                if DeltaE<=0:                                    # here metropolis alg.
+                if DeltaE<=0:                                    
                     lattice[x,y,z]=lattice[x,y,z]*-1       
                 elif random.random()<(np.exp(-DeltaE/t)):   
                     lattice[x,y,z]=lattice[x,y,z]*-1 
            
             energyy = 0   
-            for i in range(size):                    #cal variables after spin flip
+            for i in range(size):                    
                 for j in range(size):
                     for k in range(size):
                         energyy=energyy+ lattice[i,j,k]*(lattice[i,(j-1)%size,k] + lattice[i,(j+1)%size,k] + lattice[(i-1)%size,j,k] + lattice[(i+1)%size,j,k] + lattice[i,j,(k+1)%size] + lattice[i,j,(k-1)%size])
@@ -52,7 +46,7 @@ def ising_model(n,tem,simulaions,J):      #tem=temperature
             energy_sq.append(((-J*energyy)/n_cube)**2)  
             
         
-        energy_tem.append(np.mean(energy[200:]))   #append the average energy per site for all simulations
+        energy_tem.append(np.mean(energy[200:]))   
     return (tem,energy_tem)
 tem = np.linspace(0.50,10.0,10)
 X = ising_model(16,tem,1000,1)
